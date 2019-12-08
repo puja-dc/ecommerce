e54634c0d636195c10183962e83aa1fa2825aed4
Puja Fitzpatrick <pujafitz@gmail.com>
2019-12-08 11:36:46 -0500

    Created productById middleware

--- controllers/product.js
+++ controllers/product.js
@@ -4,18 +4,65 @@ const fs = require('fs');
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(400).json({
+        error: 'Product not found'
+      });
+    }
+    req.product = product;
+    next();
+  });
+};
+
+exports.read = (req, res) => {
+  req.product.image = undefined;
+  return res.json(req.product);
+};
+
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
     if (err) {
       return res.status(400).json({
-        error: 'Image could not be uploaded.'
+        error: 'Image could not be uploaded'
       });
     }
+    // check for all fields
+    const {
+      name,
+      category,
+      release,
+      price,
+      quantity,
+      image,
+      shipping
+    } = fields;
+
+    // if (
+    //   !name ||
+    //   !category ||
+    //   !release ||
+    //   !price ||
+    //   !quantity ||
+    //   !image ||
+    //   !shipping
+    // ) {
+    //   return res.status(400).json({
+    //     error: 'All fields are required. Please check data and re-enter.'
+    //   });
+    // }
+
     let product = new Product(fields);
 
     if (files.image) {
+      if (files.image.size > 1000000) {
+        return res.status(400).json({
+          error: 'Image is too large. Please upload an image less than 1MB'
+        });
+      }
       product.image.data = fs.readFileSync(files.image.path);
       product.image.contentType = files.image.type;
     }
